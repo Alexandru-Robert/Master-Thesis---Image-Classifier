5f52c771407b39f06da4837e6c2ac577d3c384b7
Alexandru-Robert <andy_robert97@yahoo.com>
2021-10-30 21:24:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,27 +42,6 @@ uploaded_file = st.file_uploader("Choose a product image ...")#, type="jpeg")
 
 #need to see how to do if single product image is uploaded. Now it classifies for all classes
 
-shoes_model = load_model('Shoes_keras_model.h5')
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-# Replace this with the path to your image
-image = Image.open(uploaded_file)
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-#turn the image into a numpy array
-image_array = np.asarray(image)
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-# Load the image into the array
-data[0] = normalized_image_array
-prediction = shoes_model.predict(data)
-print(prediction)
-
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Product image.', use_column_width=True)
@@ -70,8 +49,30 @@ if uploaded_file is not None:
     st.write("Classifying...")
 
 
-    label = teachable_machine_classification(image, 'Shoes_keras_model.h5')
+    shoes_model = load_model('Shoes_keras_model.h5')
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # Replace this with the path to your image
+    image = Image.open(uploaded_file)
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    prediction = shoes_model.predict(data)
+    print(prediction)
+
+
+
+    label = teachable_machine_classification(image, 'Shoes_keras_model.h5')
     st.write(label)
     if label == 0:
         st.write("RunFalcon 2.0")
