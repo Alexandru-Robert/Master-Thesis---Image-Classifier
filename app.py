310867cb9d22012b76588d6945125388747c6d3d
Alexandru-Robert <andy_robert97@yahoo.com>
2021-10-30 21:22:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 from PIL import Image, ImageOps
 from img_classification import teachable_machine_classification
 from explore_page import show_explore_page
+import numpy as np
 
 st.set_page_config(page_title="MasterThesis", page_icon=None, layout='centered', initial_sidebar_state='auto')
 # favicon being an object of the same kind as the one you should provide st.image() with (ie. a PIL array for example) or a string (url or local file path)
@@ -41,6 +42,25 @@ uploaded_file = st.file_uploader("Choose a product image ...")#, type="jpeg")
 
 #need to see how to do if single product image is uploaded. Now it classifies for all classes
 
+shoes_model = load_model('Shoes_keras_model.h5')
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+# Replace this with the path to your image
+image = Image.open(uploaded_file)
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+# Load the image into the array
+data[0] = normalized_image_array
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Product image.', use_column_width=True)
@@ -49,7 +69,6 @@ if uploaded_file is not None:
 
 
     label = teachable_machine_classification(image, 'Shoes_keras_model.h5')
-    shoes_model = load_model('Shoes_keras_model.h5')
     prediction = shoes_model.predict(image)
     print(prediction)
     st.write(label)
