2e8dd7b38123e1f73736e93734263e1c4015e123
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-04 18:47:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -210,10 +210,8 @@ with st.container():
     col1, col2, col3 = st.columns(3)
      with col1:
        st.header("A cat")
-
      with col2:
         st.header("A dog")
-
      with col3:
         st.header("An owl")
 
