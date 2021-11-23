d953355dd770f895625cd43f4fd926d6577af79b
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-24 00:56:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -191,6 +191,7 @@ if smsb == "Single Product":
             with col1:
                 st.header("Shoes")
                 labelint = label.item()
+                st.write(labelint)
                 options = st.selectbox(
                 'What shoes model is in the picture?',
                 ['Unknown','RunFalcon 2.0', 'Supernova', 'Ultraboost 5.0 DNA', 'Ultraboost 21','X9000 L3','ZG21','Adicross Retro','Adic XZ Prime Blue', 'Terrex Swift', 'Terrex Voyajer 21 Travel', 'Terrex Free Hiker Prime Blue'],
