75f9beede143521c28db4d03bdc150fe7403019a
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-24 00:48:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ if smsb == "Single Product":
             col1, col2, col3 = st.columns(3)
             with col1:
                 st.header("Shoes")
-                options = st.multiselect(
+                options = st.selectbox(
                 'What shoes model is in the picture?',
                 ['RunFalcon 2.0', 'Supernova', 'Ultraboost 5.0 DNA', 'Ultraboost 21','X9000 L3','ZG21','Adicross Retro','Adic XZ Prime Blue', 'Terrex Swift', 'Terrex Voyajer 21 Travel', 'Terrex Free Hiker Prime Blue'])
                 #st.write('You selected:', options)
