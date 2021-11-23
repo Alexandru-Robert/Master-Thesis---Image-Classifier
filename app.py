00b4d0b2fdc868f87da4e24ccc3f5c7ed9d0db9c
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-23 22:48:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ def shirts_accuracy():
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     #resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
-    image = ImageOps.fit(image, size)
+    #size = (224, 224)
+    image = ImageOps.scale(image,.5)
     st.image(image, caption='Uploaded Product image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
