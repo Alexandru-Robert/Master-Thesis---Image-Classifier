6dcd2de44044d3e9103bca24957391349382c94f
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-24 00:43:48 +0100

    function cleanup

--- app.py
+++ app.py
@@ -59,6 +59,81 @@ uploaded_file = st.file_uploader("Choose a product image ...")#, type="jpeg")
 #   notification_url = "https://mysite.example.com/notify_endpoint", 
 #   resource_type = "video")
 
+def shoes_accuracy():
+    shoes_model = load_model('Shoes_keras_model.h5')
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data.round(2)
+    # Replace this with the path to your image
+    image = Image.open(uploaded_file)
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    #labelshoes = normalized_image_array
+    prediction = shoes_model.predict(data)*100
+    confidence = np.amax(prediction) 
+    st.write(confidence,"%")
+    # st.dataframe(prediction.round(2))
+    #st.write(prediction)
+
+def pants_accuracy():
+    shoes_model = load_model('keras_modelPantsv2.h5')
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # Replace this with the path to your image
+    image = Image.open(uploaded_file)
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    prediction = shoes_model.predict(data)*100
+    confidence = np.amax(prediction) 
+    st.write(confidence,"%")
+    #st.write(prediction)
+
+def shirts_accuracy():
+    shoes_model = load_model('Shirts_keras_model.h5')
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # Replace this with the path to your image
+    image = Image.open(uploaded_file)
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    prediction = shoes_model.predict(data)*100
+    confidence = np.amax(prediction) 
+    st.write(confidence,"%")
+    #st.write(prediction)
+
 #need to see how to do if single product image is uploaded. Now it classifies for all classes
 smsb = st.sidebar.selectbox(
     "What type of pictures are there going to be added? Single product or multiple product?",
