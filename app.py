54fac9abd6809236e16abd4e8f8c3932c9e1be1b
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-24 14:00:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -459,6 +459,18 @@ if st.button('SUBMIT'):
     st.write('The image is being uploaded to the cloud wth the corrected classification')
     imageID = image
 
+# To read file as bytes: ... 
+    bytes_data = uploaded_file.getvalue() 
+    st.write(bytes_data)
+
+# To convert to a string based IO: 
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+# To read file as string: ... 
+    string_data = stringio.read() 
+    st.write(string_data)
+
     with BytesIO() as buf:
         image.save(buf, 'jpeg')
         image_bytes = buf.getvalue()
