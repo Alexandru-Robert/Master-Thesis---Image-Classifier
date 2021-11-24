ffc8d9043c255afc243f5cdbfc0146ca71e3e1d3
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-24 22:31:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def shoes_accuracy():
     # The 'length' or number of images you can put into the array is
     # determined by the first position in the shape tuple, in this case 1.
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    data.round(2)
+    #data.round(2)
     # Replace this with the path to your image
     image = Image.open(uploaded_file)
     #resize the image to a 224x224 with the same strategy as in TM2:
