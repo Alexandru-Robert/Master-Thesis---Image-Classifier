26eacbf1a1d81cbb53e88b1bc1dd8c7d37693cb3
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-24 21:06:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -465,9 +465,9 @@ if st.button('SUBMIT'):
     cloudinary.uploader.upload(image_bytes, 
     folder = "SampleImages/" + imageTags +"/", 
     tags = imageTags,
-    public_id = imageTitle#+str(randomID),
+    public_id = imageTitle+str(randomID),
+    title = imageTitle
     #overwrite = true, 
     #notification_url = "https://mysite.example.com/notify_endpoint", 
     #resource_type = "image"
     )
-    # cloudinary.uploader.add_tag(imageTags, imageTitle)#+str(randomID))
