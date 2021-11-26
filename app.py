c5322d3396c1245517f6ae1060604e36735e790a
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-26 16:28:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,45 @@ globalLabelShirts = 0
 globalLabelPants = 0
 globalLabelShoes = 0
 
+
+# globalConfidence = 0
+global confidence
+
+def teachable_machine_classification(img, weights_file):
+    # Load the model
+    model = keras.models.load_model(weights_file, compile=False)
+
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = img
+    #image sizing
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    predictionPerc = prediction*100
+    #global confidence 
+    confidence = np.amax(predictionPerc) 
+
+    # confidencePerc(confidence)
+    #st.write(np.argmax(prediction))
+    return np.argmax(prediction) # return position of the highest probability
+
+st.write(confidence)
+
+def confidencePerc():
+    st.write(confidence,"%")
+
+
 def shoes_accuracy():
     shoes_model = load_model('Shoes_keras_model.h5')
     # Create the array of the right shape to feed into the keras model
@@ -467,39 +506,3 @@ else:
         )
 
 
-# globalConfidence = 0
-global confidence
-
-def teachable_machine_classification(img, weights_file):
-    # Load the model
-    model = keras.models.load_model(weights_file, compile=False)
-
-    # Create the array of the right shape to feed into the keras model
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    predictionPerc = prediction*100
-    #global confidence 
-    confidence = np.amax(predictionPerc) 
-
-    # confidencePerc(confidence)
-    #st.write(np.argmax(prediction))
-    return np.argmax(prediction) # return position of the highest probability
-
-st.write(confidence)
-
-def confidencePerc():
-    st.write(confidence,"%")
