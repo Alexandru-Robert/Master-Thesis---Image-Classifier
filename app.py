9159ea0afbca09b1c1b87a51878d5f72dcd14600
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-26 16:17:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -368,7 +368,7 @@ else:
             with col2:
                 #confidencePerc()
                 #teachable_machine_classification()
-                st.write(label[9], "%")
+                st.write(label[0], "%")
                 #shoes_accuracy()    
         #PANTS LABEL & ACC
         with st.container():
