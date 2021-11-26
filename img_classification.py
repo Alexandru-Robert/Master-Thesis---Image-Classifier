a260bd0f11f91ea626f39fff626f3bf936d15f80
Alexandru-Robert <andy_robert97@yahoo.com>
2021-11-26 16:24:49 +0100

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -36,6 +36,8 @@ def teachable_machine_classification(img, weights_file):
     #st.write(np.argmax(prediction))
     return np.argmax(prediction) # return position of the highest probability
 
+print(confidence)
+
 def confidencePerc():
     st.write(confidence,"%")
     #globalConfidence = x
